ddd2f85b1a75063ab50224a76fd5cb40a50d5560
mxrpheus6 <pkazachenkoo@gmail.com>
2025-10-17 18:24:10 +0300

    Excluded security filter

--- src/main/java/com/innowise/apigateway/client/auth/TokenValidationResponse.java
+++
@@ -1,6 +0,0 @@
-package com.innowise.apigateway.client.auth;
-
-public record TokenValidationResponse(
-        boolean success
-) {
-}
--- src/main/java/com/innowise/apigateway/configuration/GatewayConfig.java
+++ src/main/java/com/innowise/apigateway/configuration/GatewayConfig.java
@@ -1,7 +1,5 @@
 package com.innowise.apigateway.configuration;
 
-import com.innowise.apigateway.constants.ApplicationConstants;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -11,10 +9,7 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@RequiredArgsConstructor
 public class GatewayConfig {
-
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Value("${service.auth.url}")
     private String authServiceUrl;
@@ -27,18 +22,12 @@ public class GatewayConfig {
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
-        var config = new JwtAuthenticationFilter.Config();
-        config.setPublicEndpoints(ApplicationConstants.PUBLIC_ENDPOINTS);
-
         return builder.routes()
                 .route("auth-service", r -> r.path("/api/v1/auth/**")
-                        .filters(f -> f.filter(jwtAuthenticationFilter.apply(config)))
                         .uri(authServiceUrl))
                 .route("order-service", r -> r.path("/api/v1/orders/**")
-                        .filters(f -> f.filter(jwtAuthenticationFilter.apply(config)))
                         .uri(orderServiceUrl))
-                .route("user-service", r -> r.path("/api/v1/products/**")
-                        .filters(f -> f.filter(jwtAuthenticationFilter.apply(config)))
+                .route("user-service", r -> r.path("/api/v1/users/**")
                         .uri(userServiceUrl))
                 .build();
     }
